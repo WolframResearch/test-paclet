e50c716ff57ade478fe2e34a1e6e6f24e282b7fd
Rick Hennigan <richardh@wolfram.com>
2022-02-17 15:22:56 -0500

    Initial commit

---
+++ src/main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+export SCRIPT_DIR=$(dirname ${0})
+
+echo "::group::Installing dependencies..."
+wolframscript ${SCRIPT_DIR}/install_dependencies.wls
+echo "::endgroup::"
+
+echo "::group::Checking Paclet..."
+wolframscript ${SCRIPT_DIR}/test_paclet.wls
+echo "::endgroup::"
